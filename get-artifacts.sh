f2c55c00ab7ee2363dc68b804ff77265a63224e0
Dheeraj.Vadde <Dheeraj.Vadde@armedia.com>
2021-08-16 23:26:18 +0530

    pentahoEE

--- get-artifacts.sh
+++ get-artifacts.sh
@@ -14,12 +14,12 @@ ARKCASE_PRE_AUTH_VERSION="4-1.1.1"
 rm -rf artifacts
 mkdir artifacts
 echo "Downloading  Pentaho EE artifacts version  $Pentaho_Version"
-aws s3 cp  "s3://arkcase-container-artifacts/ark_pentaho_ee/pentaho-server-ee-${PENTAHO_SERVER_EE}-dist.zip" --profile armedia artifacts/
-aws s3 cp  "s3://arkcase-container-artifacts/ark_pentaho_ee/pentaho-server-licenses-${PENTAHO_SERVER_LICENSES}.zip" --profile armedia artifacts/
-aws s3 cp  "s3://arkcase-container-artifacts/ark_pentaho_ee/pir-plugin-ee-${PIR_PLUGIN_EE}-dist.zip" --profile armedia artifacts/
-aws s3 cp  "s3://arkcase-container-artifacts/ark_pentaho_ee/paz-plugin-ee-${PAZ_PLUGIN_EE}-dist.zip" --profile armedia artifacts/
-aws s3 cp  "s3://arkcase-container-artifacts/ark_pentaho_ee/pdd-plugin-ee-${PDD_PLUGIN_EE}-dist.zip" --profile armedia artifacts/
-aws s3 cp  "s3://arkcase-container-artifacts/ark_pentaho_ee/pdi-ee-client-${PDI_EE_CLIENT}-dist.zip" --profile armedia artifacts/
-aws s3 cp  "s3://arkcase-container-artifacts/ark_pentaho_ee/mysql-connector-java-${MYSQL_CONNECTOR_VERSION}.jar" --profile armedia artifacts/
-aws s3 cp "s3://arkcase-container-artifacts/ark_pentaho_ee/mariadb-java-client-${MARIADB_CONNECTOR_VERSION}.jar" --profile armedia artifacts/
-aws s3 cp "s3://arkcase-container-artifacts/ark_pentaho_ee/arkcase-preauth-springsec-v${ARKCASE_PRE_AUTH_VERSION}-bundled.jar" --profile armedia artifacts/
+aws s3 cp  "s3://arkcase-container-artifacts/ark_pentaho_ee/pentaho-server-ee-${PENTAHO_SERVER_EE}-dist.zip" artifacts/
+aws s3 cp  "s3://arkcase-container-artifacts/ark_pentaho_ee/pentaho-server-licenses-${PENTAHO_SERVER_LICENSES}.zip" artifacts/
+aws s3 cp  "s3://arkcase-container-artifacts/ark_pentaho_ee/pir-plugin-ee-${PIR_PLUGIN_EE}-dist.zip" artifacts/
+aws s3 cp  "s3://arkcase-container-artifacts/ark_pentaho_ee/paz-plugin-ee-${PAZ_PLUGIN_EE}-dist.zip" artifacts/
+aws s3 cp  "s3://arkcase-container-artifacts/ark_pentaho_ee/pdd-plugin-ee-${PDD_PLUGIN_EE}-dist.zip" artifacts/
+aws s3 cp  "s3://arkcase-container-artifacts/ark_pentaho_ee/pdi-ee-client-${PDI_EE_CLIENT}-dist.zip" artifacts/
+aws s3 cp  "s3://arkcase-container-artifacts/ark_pentaho_ee/mysql-connector-java-${MYSQL_CONNECTOR_VERSION}.jar" artifacts/
+aws s3 cp "s3://arkcase-container-artifacts/ark_pentaho_ee/mariadb-java-client-${MARIADB_CONNECTOR_VERSION}.jar" artifacts/
+aws s3 cp "s3://arkcase-container-artifacts/ark_pentaho_ee/arkcase-preauth-springsec-v${ARKCASE_PRE_AUTH_VERSION}-bundled.jar" artifacts/
